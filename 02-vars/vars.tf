06bb6107af38b86c91fc0f4cf33ef828c0fbcbcd
vijay <bvsreddy703@gmail.com>
2022-03-30 10:52:17 +0530

    Updating terraform

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -21,3 +21,25 @@ variable "sample2" {
 variable "sample3" {
   default = true
 }
+
+variable "list" {
+  default = ["Hello", 90, true]
+}
+# List can have values of different data types.
+
+variable "map" {
+  default = {
+    string  = "Hello",
+    number  = 100,
+    boolean = true
+  }
+}
+
+## Variable values from tfvars
+variable "new" {}
+
+## Value from CLI
+variable "cli" {}
+
+
+variable "shell" {}
