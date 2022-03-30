d78f741a707b972004f70892909186f2ae2be61d
vijay <bvsreddy703@gmail.com>
2022-03-30 17:54:31 +0530

    Updating terraform

--- roboshop/module/ec2.tf
+++ roboshop/module/ec2.tf
@@ -1,4 +1,14 @@
-resource "aws_instance" "ec2" {
+#resource "aws_instance" "ec2" {
+#  ami                    = "ami-0bb6af715826253bf"
+#  instance_type          = "t3.micro"
+#  vpc_security_group_ids = [aws_security_group.allow_app.id]
+#
+#  tags = {
+#    Name = var.COMPONENT
+#  }
+#}
+
+resource "aws_spot_instance_request" "cheap_worker" {
   ami                    = "ami-0bb6af715826253bf"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_app.id]
@@ -6,4 +16,5 @@ resource "aws_instance" "ec2" {
   tags = {
     Name = var.COMPONENT
   }
+
 }
