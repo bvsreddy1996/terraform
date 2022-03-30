027b033027ea786de0cb1127ad980f4214e2a2a3
vijay <bvsreddy703@gmail.com>
2022-03-30 10:57:48 +0530

    Updating terraform

--- 02-vars/outputs.tf
+++ 02-vars/outputs.tf
@@ -2,3 +2,26 @@ output "sample" {
   value = var.sample
 }
 
+output "list1" {
+  value = var.list[0]
+}
+
+output "list2" {
+  value = var.list[1]
+}
+
+output "map_string" {
+  value = var.map["string"]
+}
+
+output "new" {
+  value = var.new
+}
+
+output "cli" {
+  value = var.cli
+}
+
+output "shell" {
+  value = var.shell
+}
