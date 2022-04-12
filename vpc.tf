b16f3fb9a84a2381fd92029c5ee48ada46c0ba10
vijay <bvsreddy703@gmail.com>
2022-04-12 12:18:39 +0530

    Updating terraform

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "vpc" {
   cidr_block       = "${var.vpc-cidr}"
   instance_tenancy = "default"
   enable_dns_hostnames = true
@@ -9,7 +9,7 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_internet_gateway" "internet-gateway" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.vpc.id
 
   tags = {
     Name = "Test-IGW"
@@ -17,7 +17,7 @@ resource "aws_internet_gateway" "internet-gateway" {
 }
 
 resource "aws_subnet" "public-subnet-1" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.vpc.id
   cidr_block = "${var.public-subnet-1-cidr}"
   availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
@@ -28,7 +28,7 @@ resource "aws_subnet" "public-subnet-1" {
 }
 
 resource "aws_subnet" "public-subnet-2" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.vpc.id
   cidr_block = "${var.public-subnet-2-cidr}"
   availability_zone = "us-east-1b"
   map_public_ip_on_launch = true
@@ -39,14 +39,72 @@ resource "aws_subnet" "public-subnet-2" {
 }
 
 resource "aws_route_table" "public-route-table" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.vpc.id
 
   route = {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "aws_internet_gateway.internet-gateway.id"
+    gateway_id = aws_internet_gateway.internet-gateway.id
   }
 
   tags = {
-    Name = "example"
+    Name = "Public RouteTable"
   }
 }
+
+resource "aws_route_table_association" "public-subnet-1-route-table-association" {
+  subnet_id = aws_subnet.public-subnet-1.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+resource "aws_route_table_association" "public-subnet-2-route-table-association" {
+  subnet_id = aws_subnet.public-subnet-2.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+resource "aws_subnet" "private-subnet-1" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "${var.private-subnet-1-cidr}"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "Private Subnet 1 | App Tier"
+  }
+}
+
+resource "aws_subnet" "private-subnet-2" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "${var.private-subnet-2-cidr}"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "Private Subnet 2 | App Tier"
+  }
+}
+
+resource "aws_subnet" "private-subnet-3" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "${var.private-subnet-3-cidr}"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "Private Subnet 3 | App Tier"
+  }
+}
+
+resource "aws_subnet" "private-subnet-4" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "${var.private-subnet-4-cidr}"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "Private Subnet 4 | App Tier"
+  }
+}
+
+
+
+
